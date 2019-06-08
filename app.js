185b2dc7fe16b233a6615251f9b41ae6918b1581
Burak Unuvar <burakunuvar@gmail.com>
2019-06-08 15:27:35 +0300

    Level 4 - Salting and Hashing with bcrypt

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 //jshint esversion:6
 require('dotenv').config();
-const express=require("express");
-const bodyParser=require("body-parser");
-const ejs=require("ejs");
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const mongoose = require('mongoose');
-var md5 = require('md5');
+// var md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 const User = require("./models/user");
 
-mongoose.connect('mongodb://localhost:27017/userDBforAuth', {useNewUrlParser: true});
+
+mongoose.connect('mongodb://localhost:27017/userDBforAuth', {
+  useNewUrlParser: true
+});
 
 app = express();
 app.use(express.static("public"));
@@ -29,35 +34,41 @@ app.get("/register", function(req, res) {
 });
 
 app.post("/register", function(req, res) {
-  const newUser = new User({
+  bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+    const newUser = new User({
       email: req.body.username,
-      password: md5(req.body.password)
+      password: hash
     });
-  newUser.save(function(err){
-    if(!err){
-      res.render("secret");
-    }else{
-      console.log(err);
-    }
+    newUser.save(function(err) {
+      if (!err) {
+        res.render("secret");
+      } else {
+        console.log(err);
+      }
+    });
+    // Store hash in your password DB.
   });
 
 });
 
-app.post("/login",function(req,res){
+app.post("/login", function(req, res) {
   const email = req.body.username;
-  const password = md5(req.body.password);
-  User.findOne({email:email},function(err,foundUser){
-    if(!err){
-      if(foundUser){
-        if(foundUser.password === password){
-          res.render("secret");
-        }else{
-          console.log("wrong password");
-        }
+  const password = req.body.password;
+
+  User.findOne({email: email}, function(err, foundUser) {
+    if (!err) {
+      if (foundUser) {
+        bcrypt.compare(password, foundUser.password, function(err, resCompare) {
+          if (resCompare) {
+            res.render("secret");
+          } else {
+            console.log("wrong password - no match found by bcrypt");
+          }
+        });
       }else{
         console.log("no such user found");
       }
-    }else{
+    } else {
       console.log(err);
     }
   });
